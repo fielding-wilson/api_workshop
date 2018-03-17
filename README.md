a2a69dc0b84cddcc2b773c52cc5186d0fadc4963
Fielding Wilson <fielding.wilson@icloud.com>
2018-03-17 14:35:51 -0400

    cleanup and preliminary readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# api_workshop
+# API Workshop
+
+Learn REST APIs using [Node.JS](https://nodejs.org/en/), [Express](https://expressjs.com/) and [Postman](https://www.getpostman.com/apps).
+
+## Prerequisites
+1. Install Node LTS (currently 8.10.0).
+2. Install the latest version of Node Package Manager (NPM): `npm i npm -g`
+3. Install Postman. You can use the native app or the Chrome Extension
+
+## Installation
+1. Clone this repo
+2. Run: `npm install`
+3. Run: `npm start`
--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-
 const index = require('./routes/index');
 const users = require('./routes/users');
 
@@ -14,8 +12,6 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,14 +22,14 @@ app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  let err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
